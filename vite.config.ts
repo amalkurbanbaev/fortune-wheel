74db4adb97959d27dc40fe58c9b6ccd8a9169044
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2024-08-18 02:16:59 +0500

    chore: deploy configs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // eslint-disable-next-line import/no-default-export -- must be default export
 export default defineConfig({
+  base: "/fortune-wheel",
   plugins: [react()],
   resolve: {
     alias: {
