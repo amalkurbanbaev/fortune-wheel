91760a5458ad3f260f95ab15fa7d5de00f6ebc74
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2024-08-18 02:00:35 +0500

    refactor: clear layouts

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,8 @@
-import { useState } from "react";
-
-import { Button } from "./components/ui/button";
-
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-2xl font-bold">Hello!</h1>
-      <Button
-        type="button"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </Button>
-      {count}
+      <header className="border-b">header</header>
+      <main>main</main>
     </>
   );
 }
